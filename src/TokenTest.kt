871e8ff0f0fe89590333682f087f5150a8cfdf77
negitorogithub <unifarproject35@gmail.com>
2019-05-06 17:27:23 +0900

    トークンのコンストラクターのテスト追加

---
+++ src/TokenTest.kt
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+internal class TokenTest {
+    @Test
+    fun constructorTest() {
+        assertThrows<IllegalArgumentException> { Token(TokenType.NUMBER) }
+    }
+}
