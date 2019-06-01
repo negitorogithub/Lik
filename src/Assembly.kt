d06d972a04eddf0e8bd9643cf131037a9f8746c5
negitorogithub <unifarproject35@gmail.com>
2019-06-01 17:48:20 +0900

    連続して入力できるように

---
+++ src/Assembly.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    println(".intel_syntax noprefix")
+    println(".global main")
+    println("main:")
+    println("  mov rax, ${args[0]}")
+    println("  ret")
+}
