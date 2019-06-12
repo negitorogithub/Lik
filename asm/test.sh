4a12b58fea82440331e2a8d71ca9e8becb91d6f8
negitorogithub <unifarproject35@gmail.com>
2019-06-12 21:03:31 +0900

    テストシェルから一発でビルド→実行できるように

--- asm/test.sh
+++ asm/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 try() {
-  cd ../asm
+ cd ../asm
   expected="$1"
   input="$2"
   java -jar Assembly.jar "$input" > test.s
@@ -16,8 +16,13 @@ try() {
   fi
 }
 
-kotlinc Assembly.kt -include-runtime -d ../asm/Assembly.jar
-try 0 0
-try 42 42
-try 2+3 5
+cd ../src
+kotlinc *.kt -include-runtime -d ../temp
+
+cd ../temp
+jar cfm ../asm/Assembly.jar ../asm/MANIFEST.MF *.class
+
+try 0 0;
+try 42 42;
+try 5 2+3;
 echo OK
--- src/Assembly.kt
+++ src/Assembly.kt
@@ -1,18 +1,23 @@
-fun main(args: Array<String>) {
-    val buffer = StringBuilder()
-    args.forEach {
-        buffer.append(it)
+class Assembly {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val buffer = StringBuilder()
+            args.forEach {
+                buffer.append(it)
+            }
+            println(".intel_syntax noprefix")
+            println(".global main")
+            println("main:")
+            Nodes(
+                Tokens(
+                    tokenize(
+                        buffer.toString()
+                    )
+                ).parse()
+            ).printAssemblies()
+            println("  ret")
+        }
     }
-    println(".intel_syntax noprefix")
-    println(".global main")
-    println("main:")
-    Nodes(
-        Tokens(
-            tokenize(
-                buffer.toString()
-            )
-        ).parse()
-    ).printAssemblies()
-    println("  ret")
 }
 
